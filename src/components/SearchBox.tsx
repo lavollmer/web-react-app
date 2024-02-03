10dd8e5147432d576c5f13fb5a27a0b562617b29
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-02-03 12:01:04 -0600

    adding comments to the SearchBox functionality

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,8 +1,12 @@
+//import necessary dependencies
 import React from "react";
 import { CiSearch } from "react-icons/ci";
 
+//defines a type called Props
 type Props = { placeholder: string };
 
+//exported as default module - component accepts placeholder prop
+//SearchBox has a form element with input and a button that passes the placeholder prop through
 export default function SearchBox({ placeholder }: Props) {
   return (
     <form className="flex relative items-center justify-center h-10">
