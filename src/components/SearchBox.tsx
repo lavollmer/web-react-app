6b933d0d11d173736412e65dab35133233c7d7f8
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-02-03 11:52:19 -0600

    Added search icon from React icons

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { CiSearch } from "react-icons/ci";
 
 type Props = {};
 
 export default function SearchBox({}: Props) {
   return (
     <form className="flex relative items-center justify-center h-10">
-      Search Box
+      <input>
+        <button>
+          <CiSearch />
+        </button>
+      </input>
     </form>
   );
 }
