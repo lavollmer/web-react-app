cd80796d64a9397804f230e61ab3f3bc4a61324e
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-02-03 11:50:53 -0600

    added Search Box

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,28 @@
-import React from 'react'
+import React from "react";
 import { MdWbSunny } from "react-icons/md";
 import { FaLocationCrosshairs } from "react-icons/fa6";
+import { MdLocationOn } from "react-icons/md";
 
-type Props = {}
+type Props = {};
 
 export default function Navbar({}: Props) {
   return (
     // sticky = stick navbar on position, left-0 position of navbar
-    <nav className=' shadow-sm sticky top-0 z-50 bg-white'>
-      <div className='h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto'>
-        <p className='flex items-center justify-center gap-2'>
-          <h2 className='text-gray-500 text-3xl'>
+    <nav className=" shadow-sm sticky top-0 z-50 bg-white">
+      <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
+        <p className="flex items-center justify-center gap-2">
+          <h2 className="text-gray-500 text-3xl">
             Weather
-            <MdWbSunny className='text-3xl mt-1 text-yellow-300' />
+            <MdWbSunny className="text-3xl mt-1 text-yellow-300" />
           </h2>
         </p>
-        <section className='flex gap-2 items-center'>
-<FaLocationCrosshairs className='text-2xl text-gray-400 hover:opacity-80 cursor-pointer' />
+        <section className="flex gap-2 items-center">
+          <FaLocationCrosshairs className="text-2xl text-gray-400 hover:opacity-80 cursor-pointer" />
+          <MdLocationOn className="text-3xl" />
+          <p className="text-slate-900/80 text-sm"> Minnesota</p>
+          <div>{/* Search Box */}</div>
         </section>
       </div>
-</nav>
-  )
+    </nav>
+  );
 }
---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+export default function SearchBox({}: Props) {
+  return (
+    <form className="flex relative items-center justify-center h-10">
+      Search Box
+    </form>
+  );
+}
