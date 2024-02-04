8c1f13ccfa217b5558cc6880504c4f34cbd98bd2
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-02-04 11:28:28 -0600

    added comments to the stlying in SearchBox

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -24,6 +24,8 @@ export default function SearchBox({ placeholder }: Props) {
         // h-full: Sets the height of the element to 100% of its parent container.
         className="px-4 py-2 w-[230px] border border-gray-300 rounded-1-md focus:outline-none focus:border-blue-500 h-full"
       />
+      {/* px-4 sets the horizontal padding of an element to 4 units, bg-blue-500 sets the background color of an element to a shade of blue, text-white sets the text color of an element to white, rounded-r-md applies rounded corners to the right side of an element, focus:outline-none removes the outline that appears around an element when it is in focus, hover:bg-blue-600 changes the background color of an element to a different shade of blue when the element is being hovered over., whitespace-nowrap prevents text from wrapping to the next line and keeps it on a single line., 
+h-full sets the height of an element to 100% of its parent container. */}
       <button>
         <CiSearch className="px-4 py-[9x] bg-blue-500 text-white rounded-r-md focus:outline-none hover:bg-blue-600 whitespace-nowrap h-full" />
       </button>
