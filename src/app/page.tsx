389dce1ebf61b0c7c73d1b367c028c8c0b345fbe
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-02-06 09:06:15 -0600

    debugging home page with export issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
 import Navbar from "@/components/Navbar";
 
 //api.openweathermap.org/data/2.5/weather?q=minneapolis&appid={02198656acbfe2c2afe6c0d052bc513c}
-https: export default function Home() {
+export default function Home() {
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
