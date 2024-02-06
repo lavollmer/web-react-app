2f720941ada015ac6eb046727d8029afddd680e3
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-02-06 09:01:07 -0600

    added API key from open weather map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
 
-export default function Home() {
+//api.openweathermap.org/data/2.5/weather?q=minneapolis&appid={02198656acbfe2c2afe6c0d052bc513c}
+https: export default function Home() {
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
-      </div>
+    </div>
   );
 }
