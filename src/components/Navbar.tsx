9fd28a336d9d73c458cba9d75be3d0c4db560e1c
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-02-06 09:15:02 -0600

    adding comments to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,11 @@ import { FaLocationCrosshairs } from "react-icons/fa6";
 import { MdLocationOn } from "react-icons/md";
 import SearchBox from "./SearchBox";
 
+// type Props = { ... }: This line declares a new type called Props using the type keyword. The type is an object type, indicated by the curly braces {}.
+// className?: string;: This line defines an optional prop called className of type string. The ? indicates that the prop is optional and can be omitted when using the Navbar component.
+// onChange: React.ChangeEventHandler<HTMLInputElement> | undefined;: This line defines a required prop called onChange of type React.ChangeEventHandler<HTMLInputElement>. This prop is a function that handles the change event of an HTML input element. The | undefined allows the prop to be undefined if not provided.
+// value: string;: This line defines a required prop called value of type string. This prop represents the value of an input element.
+// onSubmit: React.FormEventHandler<HTMLFormElement> | undefined;: This line defines an optional prop called onSubmit of type React.FormEventHandler<HTMLFormElement>. This prop is a function that handles the submit event of an HTML form element. The | undefined allows the prop to be undefined if not provided.
 type Props = {
   className?: string;
   // placeholder: string;
